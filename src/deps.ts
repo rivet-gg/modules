a564fead4402c5b0a6bb93148b2abc6cf2d999b2
Nathan Flurry <git@nathanflurry.com>
2024-03-04 15:47:57 -0800

    feat: auto-create postgres container

--- src/deps.ts
+++ src/deps.ts
@@ -8,4 +8,4 @@ export {
 } from "https://deno.land/std@0.208.0/path/mod.ts";
 export { copy, exists, emptyDir } from "https://deno.land/std@0.208.0/fs/mod.ts";
 export { parse, stringify } from "https://deno.land/std@0.208.0/yaml/mod.ts";
-export { assertEquals, assertExists } from "https://deno.land/std@0.208.0/assert/mod.ts";
+export { assert, assertEquals, assertExists } from "https://deno.land/std@0.208.0/assert/mod.ts";
