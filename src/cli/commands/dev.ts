8199ba97bd68fb5a4b636e3da918aa20baa5a5e4
Nathan Flurry <NathanFlurry@users.noreply.github.com>
2024-03-12 19:21:26 -0700

    feat: add watch mode to start & build & test commands (#205)

--- src/cli/commands/dev.ts
+++ src/cli/commands/dev.ts
@@ -3,49 +3,53 @@ import { Command } from "../deps.ts";
 import { GlobalOpts, initProject } from "../common.ts";
 import { build, DbDriver, Format, Runtime } from "../../build/mod.ts";
 import { ensurePostgresRunning } from "../../utils/postgres_daemon.ts";
+import { watch } from "../../watch/mod.ts";
+import { Project } from "../../project/mod.ts";
 
 export const devCommand = new Command<GlobalOpts>()
 	.description("Start the development server")
 	.option("--no-build", "Don't build source files")
 	.option("--no-check", "Don't check source files before running")
-	.option("--unstable-watch", "Automatically restart server on changes. This does not support all features yet.")
+	.option("--no-watch", "Automatically restart server on changes")
 	.action(
 		async (opts) => {
 			const project = await initProject(opts);
 
 			await ensurePostgresRunning(project);
 
-			const entrypointPath = resolve(project.path, "_gen", "entrypoint.ts");
+			await watch(project, {
+				disableWatch: !opts.watch,
+				async fn(project: Project) {
+					// Build project
+					if (opts.build) {
+						await build(project, {
+							runtime: Runtime.Deno,
+							format: Format.Native,
+							dbDriver: DbDriver.NodePostgres,
+						});
+					}
 
-			// Build project
-			if (opts.build) {
-				await build(project, {
-					runtime: Runtime.Deno,
-					format: Format.Native,
-					dbDriver: DbDriver.NodePostgres,
-				});
-			}
+					// Determine args
+					const args = [
+						"--allow-env",
+						"--allow-net",
+						"--allow-read",
+					];
+					if (opts.check) args.push("--check");
 
-			// Determine args
-			const args = [
-				"--allow-env",
-				"--allow-net",
-				"--allow-read",
-			];
-			if (opts.check) args.push("--check");
-			if (opts.unstableWatch) args.push("--watch");
-
-			// Run entrypoint
-			const cmd = await new Deno.Command("deno", {
-				args: [
-					"run",
-					...args,
-					entrypointPath,
-				],
-				stdout: "inherit",
-				stderr: "inherit",
-			})
-				.output();
-			if (!cmd.success) throw new Error("Entrypoint failed");
+					// Run entrypoint
+					const cmd = await new Deno.Command("deno", {
+						args: [
+							"run",
+							...args,
+							resolve(project.path, "_gen", "entrypoint.ts"),
+						],
+						stdout: "inherit",
+						stderr: "inherit",
+					})
+						.output();
+					if (!cmd.success) throw new Error("Entrypoint failed");
+				},
+			});
 		},
 	);
---
+++ src/watch/mod.ts
@@ -0,0 +1,87 @@
+import { abortable } from "./deps.ts";
+import { resolve, SEP } from "../deps.ts";
+import { Project } from "../project/mod.ts";
+import { loadProject } from "../project/project.ts";
+import { info, verbose } from "../term/status.ts";
+
+export interface WatchOpts {
+	/**
+	 * If true, don't watch for changes and just runs `fn` once.
+	 *
+	 * This is useful for keeping logic simple where watching needs to be
+	 * disabled.
+	 */
+	disableWatch?: boolean;
+
+	fn: (project: Project, abortController: AbortController) => Promise<void>;
+}
+
+export async function watch(initProject: Project, opts: WatchOpts) {
+	// TODO: debounce
+	// TODO: catch errors
+
+	if (opts.disableWatch) {
+		await opts.fn(initProject, new AbortController());
+		return;
+	}
+
+	let project = initProject;
+	let fnAbortController: AbortController | null = null;
+	while (true) {
+		// Run action
+		fnAbortController = new AbortController();
+		abortable(opts.fn(project, fnAbortController), fnAbortController.signal);
+
+		// Wait for change that we care about
+		let foundEvent = false;
+		const watchPaths = getWatchPaths(project);
+		const watcher = Deno.watchFs(watchPaths);
+		verbose("Watching", watchPaths.join(", "));
+		for await (const event of watcher) {
+			verbose("Watch event", JSON.stringify(event));
+			const relevantPaths = event.paths.filter(shouldPathTriggerRebuild);
+			if (relevantPaths.length > 0) {
+				foundEvent = true;
+				info("Change detected", relevantPaths.join(", "));
+				break;
+			}
+		}
+		if (!foundEvent) {
+			throw new Error("Unreachable: watchFs iterator ended");
+		}
+
+		// Abort previous build. Ignore if it's already aborted.
+		try {
+			fnAbortController?.abort();
+		} catch (err) {
+			if (err.name != "AbortError") throw err;
+		}
+
+		// Reload project
+		project = await loadProject({ path: project.path });
+	}
+}
+
+function getWatchPaths(project: Project) {
+	const paths: string[] = [
+		resolve(project.path, "backend.yaml"),
+	];
+	for (const module of project.modules.values()) {
+		if (!("local" in module.registry.config) || module.registry.config.local.isExternal) continue;
+		paths.push(module.path);
+	}
+	return paths;
+}
+
+function shouldPathTriggerRebuild(path: string) {
+	const pathSplit = path.split(SEP);
+
+	// Ignore generated files
+	if (pathSplit.includes("_gen")) return false;
+
+	// Prisma touches the db/migrations automatically, so we don't want to rebuild as a side effect
+	const dbIdx = pathSplit.indexOf("db");
+	if (dbIdx != -1 && pathSplit[dbIdx + 1] == "migrations") return false;
+
+	return true;
+}
