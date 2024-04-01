5102d1140b89d66e87e773feadebf3812eae0ddb
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2024-04-01 12:53:08 -0400

    feat: Allow interpolation of `env` variables into `backend.yaml`

--- src/config/project.ts
+++ src/config/project.ts
@@ -2,6 +2,8 @@ import { parse, resolve } from "../deps.ts";
 import { Ajv } from "./deps.ts";
 import schema from "../../artifacts/project_schema.json" with { type: "json" };
 import { InternalError } from "../error/mod.ts";
+import { load as loadEnv } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
+import { verbose, warn } from "../term/status.ts";
 
 export interface ProjectConfig extends Record<string, unknown> {
 	registries: { [name: string]: RegistryConfig };
@@ -62,11 +64,21 @@ const projectConfigAjv = new Ajv.default({
 });
 
 export async function readConfig(projectPath: string): Promise<ProjectConfig> {
+	// Load `.env` file
+	const envPath = dotenvPath(projectPath);
+	try {
+		verbose(`Loading .env file`, envPath);
+		await loadEnv({ envPath, export: true });
+	} catch (e) {
+		warn("Failed to load .env file", e);
+	}
+
 	// Read config
 	const configRaw = await Deno.readTextFile(
 		configPath(projectPath),
 	);
-	const config = parse(configRaw) as ProjectConfig;
+	const rawConfig = parse(configRaw);
+	const config = interpolateEnvVars(rawConfig);
 
 	// Validate config
 	const projectConfigSchema = projectConfigAjv.getSchema("#");
@@ -79,9 +91,90 @@ export async function readConfig(projectPath: string): Promise<ProjectConfig> {
 		);
 	}
 
-	return config;
+	return config as ProjectConfig;
+}
+
+function interpolateEnvVars(config: unknown): unknown {
+	function isObject(value: unknown): value is Record<string, unknown> {
+		return typeof value === "object" && value !== null;
+	}
+
+	function replaceEnvObjects(value: unknown): unknown {
+		if (!isObject(value)) {
+			if (Array.isArray(value)) {
+				// Recursively replace env objects in arrays
+				return value.map(replaceEnvObjects);
+			} else {
+				// Value is a scalar, return it as is.
+				return value;
+			}
+		}
+
+		// If value is of type `{ env: "ENV_VAR_NAME" }`
+		if (Object.keys(value).length === 1 && "env" in value) {
+			// Get the environment variable name
+			const envVarName = value["env"];
+			if (typeof envVarName !== "string") {
+				throw new Error("env field must be a string");
+			}
+
+			// Get the environment variable value
+			const envVar = Deno.env.get(envVarName);
+			if (envVar === undefined) {
+				throw new Error(`Environment variable ${value["env"]} is not set`);
+			}
+
+			// Return the environment variable value as the canonicalized value of the
+			// { env: "ENV_VAR_NAME" } object
+			return envVar;
+		} else {
+			// Recursively replace env objects in objects
+			const newValue: Record<string, unknown> = {};
+			for (const key in value) {
+				newValue[key] = replaceEnvObjects(value[key]);
+			}
+
+			// Return the recursively interpolated object
+			return newValue;
+		}
+	}
+
+	if (!isObject(config)) return config;
+
+	const modules = config["modules"];
+	if (!isObject(modules)) return config;
+
+	const newModules: Record<string, unknown> = {};
+	for (const moduleName in modules) {
+		// Get the module
+		const module = modules[moduleName];
+		if (!isObject(module)) {
+			newModules[moduleName] = module;
+			continue;
+		}
+
+		// Get the `config` field of each module
+		const moduleConfig = module["config"];
+
+		// Replace the old config with the new one
+		const newModule = {
+			...module,
+			config: replaceEnvObjects(moduleConfig),
+		};
+		newModules[moduleName] = newModule;
+	}
+
+	// Replace the old modules with the ones with new configs
+	return {
+		...config,
+		modules: newModules,
+	};
 }
 
 export function configPath(root: string): string {
 	return resolve(root, "backend.yaml");
+}
+
+export function dotenvPath(root: string): string {
+	return resolve(root, ".env");
 }
