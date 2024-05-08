0e6a3467f07cb3f87ae256adb4137b446cb1e9b4
ABCxFF <79597906+ABCxFF@users.noreply.github.com>
2024-05-08 12:15:39 -0400

    feat(auth/tests): e2e verifying correct user ids

--- modules/auth/tests/e2e.ts
+++ modules/auth/tests/e2e.ts
@@ -20,3 +20,37 @@ test("e2e", async (ctx: TestContext) => {
 	});
 	assertEquals(verifyRes.token.type, "user");
 });
+
+
+test("e2e with user token", async (ctx: TestContext) => {
+	const { user } = await ctx.modules.users.createUser({});
+
+	const { token: session } = await ctx.modules.users.createUserToken({
+		userId: user.id
+	});
+
+	const authRes = await ctx.modules.auth.authEmailPasswordless({
+		email: faker.internet.email(),
+		userToken: session.token
+	});
+
+	// Look up correct code
+	const { code } = await ctx.db.emailPasswordlessVerification.findFirstOrThrow({
+		where: {
+			id: authRes.verification.id,
+		},
+	});
+
+	const verifyRes = await ctx.modules.auth.verifyEmailPasswordless({
+		verificationId: authRes.verification.id,
+		code: code,
+	});
+
+	assertEquals(verifyRes.token.type, "user");
+
+	const verifyRes2 = await ctx.modules.users.authenticateUser({
+		userToken: verifyRes.token.token
+	});
+
+	assertEquals(verifyRes2.userId, user.id);
+});
