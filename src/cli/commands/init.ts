a85ed3b6ba5701cb57d047f22d54322129ad948b
vr-varad <varadgupta21#gmail.com>
2024-05-19 10:28:04 +0530

    Changed logs and indentation to tap size 4

--- src/cli/commands/init.ts
+++ src/cli/commands/init.ts
@@ -3,24 +3,22 @@ import { GlobalOpts } from "../common.ts";
 import { templateProject } from "../../template/project.ts";
 
 export const initCommand = new Command<GlobalOpts>()
-    .description("Create a new project")
-    .arguments("[dir]")
-    .action(
-        async (_opts, dir?: string) => {
+	.description("Create a new project")
+	.arguments("[dir]")
+	.action(
+		async (_opts, dir?: string) => {
 			// Create a new project in the specified directory
-            const createdFiles = await templateProject(dir || ".");
-
+			const createdFiles = await templateProject(dir || ".");
+			
 			// If the project creation failed, log an error message
-            if (createdFiles === undefined) {
-                console.log("Project creation failed. Please try again.");
-                return;
-            }
+			if (createdFiles === undefined) {
+				console.log("Project creation failed. Please try again.");
+				return;
+			}
 			
 			// Log the files that were created
-            console.log("Project created successfully!");
-			console.log("Files created:");
-            createdFiles.forEach(file => console.log(file));
-            console.log("Get started by following the documentation:");
-            console.log("https://opengb.dev/introduction");
-        },
-    );
+			console.log("Project created successfully!");
+			console.log("Created config file at backend.yaml");
+			console.log("Get started at https://opengb.dev/introduction");
+		},
+	);
