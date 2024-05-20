b228437bbb88e7bbd6a07431705394156de1a05f
ABCxFF <79597906+ABCxFF@users.noreply.github.com>
2024-05-20 10:32:34 -0400

    chore: tests for #85

--- modules/auth/tests/e2e.ts
+++ modules/auth/tests/e2e.ts
@@ -3,54 +3,73 @@ import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { faker } from "https://deno.land/x/deno_faker@v1.0.3/mod.ts";
 
 test("e2e", async (ctx: TestContext) => {
-	const authRes = await ctx.modules.auth.authEmailPasswordless({
-		email: faker.internet.email(),
-	});
-
-	// Look up correct code
-	const { code } = await ctx.db.emailPasswordlessVerification.findFirstOrThrow({
-		where: {
-			id: authRes.verification.id,
-		},
-	});
-
-	const verifyRes = await ctx.modules.auth.verifyEmailPasswordless({
-		verificationId: authRes.verification.id,
-		code: code,
-	});
-	assertEquals(verifyRes.token.type, "user");
-});
-
-
-test("e2e with user token", async (ctx: TestContext) => {
+	// First we create a new user, and "register" into the auth
+	// using an authEmailPasswordless({ email, userToken })
+	// call
 	const { user } = await ctx.modules.users.createUser({});
 
 	const { token: session } = await ctx.modules.users.createUserToken({
 		userId: user.id
 	});
 
-	const authRes = await ctx.modules.auth.authEmailPasswordless({
-		email: faker.internet.email(),
-		userToken: session.token
-	});
+	const fakeEmail = faker.internet.email();
 
-	// Look up correct code
-	const { code } = await ctx.db.emailPasswordlessVerification.findFirstOrThrow({
-		where: {
-			id: authRes.verification.id,
-		},
-	});
+	// Now we test that post-signin, we get the same user
+	{
+		const authRes = await ctx.modules.auth.authEmailPasswordless({
+			email: fakeEmail,
+			userToken: session.token
+		});
 
-	const verifyRes = await ctx.modules.auth.verifyEmailPasswordless({
-		verificationId: authRes.verification.id,
-		code: code,
-	});
+		// Look up correct code
+		const { code } = await ctx.db.emailPasswordlessVerification.findFirstOrThrow({
+			where: {
+				id: authRes.verification.id,
+			},
+		});
 
-	assertEquals(verifyRes.token.type, "user");
+		// Now by verifying the email, we register, and can also use
+		// this to verify the token
+		const verifyRes = await ctx.modules.auth.verifyEmailPasswordless({
+			verificationId: authRes.verification.id,
+			code: code,
+		});
 
-	const verifyRes2 = await ctx.modules.users.authenticateUser({
-		userToken: verifyRes.token.token
-	});
+		assertEquals(verifyRes.token.type, "user");
 
-	assertEquals(verifyRes2.userId, user.id);
+
+		// Make sure we end up with the same user we started with
+		const verifyRes2 = await ctx.modules.users.authenticateUser({
+			userToken: verifyRes.token.token
+		});
+
+		assertEquals(verifyRes2.userId, user.id);
+	}
+
+	// Now we try logging back in with the same email,
+	// but without a token, expecting the same user
+	{
+		const authRes = await ctx.modules.auth.authEmailPasswordless({
+			email: fakeEmail
+		});
+
+		// Look up correct code
+		const { code: code } = await ctx.db.emailPasswordlessVerification.findFirstOrThrow({
+			where: {
+				id: authRes.verification.id,
+			},
+		});
+
+		const verifyRes = await ctx.modules.auth.verifyEmailPasswordless({
+			verificationId: authRes.verification.id,
+			code: code,
+		});
+
+		const verifyRes2 = await ctx.modules.users.authenticateUser({
+			userToken: verifyRes.token.token
+		});
+
+		assertEquals(verifyRes2.userId, user.id);	
+	}
 });
+
