4ef61d5999b6c33fbf5101c523d66995435a03f8
Nathan Flurry <NathanFlurry@users.noreply.github.com>
2024-06-10 00:21:12 -0700

    chore: add metadata for uploads module (#108)

--- tests/basic/test.sh
+++ tests/basic/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X POST "https://f9c627f2-bdc-yaw--staging.opengb.rivet.gg/modules/auth/scripts/auth_email_passwordless/call" \
+curl -X POST "http://localhost:8080/modules/auth/scripts/auth_email_passwordless/call" \
   -H "Content-Type: application/json" \
   -d '{
     "email": "test@rivet.gg"
