2674ef5a7bfb835209c32569c3e6d55de4c4bab8
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2024-07-04 23:26:51 -0400

    feat: Create the `user_passwords` module

---
+++ modules/user_passwords/scripts/verify.ts
@@ -0,0 +1,37 @@
+import { Empty, RuntimeError, ScriptContext } from "../module.gen.ts";
+import { Algorithm, hashMatches } from "../utils/common.ts";
+
+export interface Request {
+	userId: string;
+    password: string;
+}
+
+export type Response = Empty;
+
+export async function run(
+	ctx: ScriptContext,
+	req: Request,
+): Promise<Response> {
+    // Look up the user password hash
+    const user = await ctx.db.passwords.findFirst({
+        where: {
+            userId: req.userId,
+        },
+        select: {
+            algo: true,
+            passwordHash: true,
+        }
+    });
+    if (!user) throw new RuntimeError("user_does_not_have_password");
+
+    // Verify the passwordHash
+    const passwordMatches = await hashMatches(
+        req.password,
+        user.passwordHash,
+        user.algo as Algorithm,
+    );
+
+    if (!passwordMatches) throw new RuntimeError("password_invalid");
+
+    return {};
+}
