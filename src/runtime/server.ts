rivet-gg/opengb

fix: move trace to body

src/runtime/server.ts
@@ -133,6 +133,7 @@ export async function handleRequest<DependenciesSnakeT, DependenciesCamelT, Acto
 		// Error response
 		const output = {
 			message: e.message,
+			trace: e.trace,
 		};
 
 		return new Response(JSON.stringify(output), {

rivet-gg/modules

src/runtime/server.ts
@@ -176,11 +176,13 @@ async function handleRequestInner<Params extends ModuleContextParams>(
 			);
 			output = {
 				message: error.message,
+				trace: error.trace,
 			};
 		} else {
 			ctx.log.error("internal error", ["error", error]);
 			output = {
 				message: "Internal error. More details have been printed in the logs.",
+				trace: error.trace,
 			};
 		}
 