AngelOnFira/opengb

feat: script API route configuration options

src/project/module.ts
@@ -4,12 +4,74 @@ import { configPath as moduleConfigPath, readConfig as readModuleConfig } from "
 import { ModuleConfig } from "../config/module.ts";
 import { Script } from "./script.ts";
 import { Actor } from "./actor.ts";
+import { Route } from "./route.ts";
 import { Project } from "./project.ts";
 import { Registry } from "./registry.ts";
 import { validateIdentifier } from "../types/identifiers/mod.ts";
 import { Casing } from "../types/identifiers/defs.ts";
-import { ProjectModuleConfig } from "../config/project.ts";
+import { configPath as projectConfigPath, ProjectModuleConfig } from "../config/project.ts";
 import { UserError } from "../error/mod.ts";
+import { RouteConfig } from "../config/module.ts";
+
+/**
+ * Validates a path provided by the user.
+ * 
+ * If the path is invalid, a {@linkcode UserError} is thrown, otherwise a version of
+ * the path without leading or trailing slashes is returned.
+ * 
+ * Paths must:
+ * - Start with a forward slash
+ * - IF they are a prefix, end with a forward slash
+ * - IF they are an exact path, NOT end with a forward slash
+ * 
+ * @param path The user-provided http path
+ * @param isPrefix Whether or not the path should be treated as a prefix
+ * @param configPath Where the (possibly) offending config file is located
+ * @returns The path without any leading or trailing slashes
+ * @throws {UserError} if the path is invalid
+ */
+function validateAndCleanPath(
+	path: string,
+	isPrefix: boolean,
+	configPath: string,
+): string {
+	// Ensure path starts with a forward slash
+	if (!path.startsWith("/")) {
+		throw new UserError(
+			"Route paths must start with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify("/" + path)}`
+			},
+		);
+	}
+
+	const hasTrailingSlash = path.endsWith("/");
+	if (isPrefix && !hasTrailingSlash) {
+		throw new UserError(
+			"Prefix paths must end with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify(path + "/")}`
+			},
+		);
+	} else if (!isPrefix && hasTrailingSlash) {
+		throw new UserError(
+			"Exact paths must not end with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify(path.replace(/\/$/, ""))}`
+			},
+		);
+	}
+
+
+	// Remove leading and trailing slashes
+	return path.replace(/^\//, "").replace(/\/$/, "");
+}
 
 export interface Module {
 	/**
@@ -49,6 +111,7 @@ export interface Module {
 
 	scripts: Map<string, Script>;
 	actors: Map<string, Actor>;
+	routes: Map<string, Route>;
 	db?: ModuleDatabase;
 
 	// Cache
@@ -60,6 +123,7 @@ export interface ModuleDatabase {
 }
 
 export async function loadModule(
+	projectRoot: string,
 	modulePath: string,
 	name: string,
 	projectModuleConfig: ProjectModuleConfig,
@@ -117,6 +181,9 @@ export async function loadModule(
 		);
 	}
 
+
+	// ACTORS
+
 	// Find names of the expected actors to find. Used to print error for extra actors.
 	const actorsPath = resolve(modulePath, "actors");
 	const expectedActors = new Set(
@@ -163,6 +230,105 @@ export async function loadModule(
 		);
 	}
 
+
+	// ROUTES
+
+	// Read routes
+	const routesPath = resolve(modulePath, "routes");
+	const expectedRoutes = new Set(
+		await glob.glob("*.ts", { cwd: resolve(modulePath, "routes") }),
+	);
+
+	const routes = new Map<string, Route>();
+	if (config.routes) {
+		let pathPrefix: string;
+		if (projectModuleConfig.routes?.pathPrefix) {
+			pathPrefix = validateAndCleanPath(
+				projectModuleConfig.routes.pathPrefix,
+				true,
+				projectConfigPath(projectRoot),
+			);
+		} else {
+			// Default to /modules/{module}/route/ for the path prefix
+			pathPrefix = validateAndCleanPath(
+				`/modules/${name}/route/`,
+				true,
+				projectConfigPath(projectRoot),
+			);
+		}
+
+		for (const routeName in config.routes) {
+			validateIdentifier(routeName, Casing.Snake);
+
+			// Load script
+			const routeScriptPath = resolve(
+				routesPath,
+				routeName + ".ts",
+			);
+			if (!await exists(routeScriptPath)) {
+				throw new UserError(
+					`Route not found at ${relative(Deno.cwd(), routeScriptPath)}.`,
+					{
+						suggest: "Check the routes in the module.yaml are configured correctly.",
+						path: moduleConfigPath(modulePath),
+					},
+				);
+			}
+
+			// Get full route path (including module prefix)
+			const relativeRouteConfig = config.routes[routeName];
+
+			// Get subpath (either path or pathPrefix)
+			let subpath: string;
+			if ("path" in relativeRouteConfig) {
+				subpath = validateAndCleanPath(
+					relativeRouteConfig.path,
+					false,
+					moduleConfigPath(modulePath),
+				);
+			} else {
+				subpath = validateAndCleanPath(
+					relativeRouteConfig.pathPrefix,
+					true,
+					moduleConfigPath(modulePath),
+				);
+			}
+
+			// Create route config with absolute path
+			let routeConfig: RouteConfig;
+			if ("path" in relativeRouteConfig) {
+				routeConfig = {
+					...relativeRouteConfig,
+					path: `/${pathPrefix}/${subpath}`,
+				};
+			} else {
+				routeConfig = {
+					...relativeRouteConfig,
+					pathPrefix: `/${pathPrefix}/${subpath}`,
+				};
+			}
+
+			const route: Route = {
+				path: routesPath,
+				name: routeName,
+				config: routeConfig,
+			};
+			routes.set(routeName, route);
+
+			// Remove script
+			expectedRoutes.delete(routeName + ".ts");
+		}
+	}
+
+	// Throw error extra routes
+	if (expectedRoutes.size > 0) {
+		const routeList = Array.from(expectedRoutes).map((x) => `- ${resolve(routesPath, x)}\n`);
+		throw new UserError(
+			`Found extra routes not registered in module.yaml.`,
+			{ details: routeList.join(""), suggest: "Add these routes to the module.yaml file.", path: routesPath },
+		);
+	}
+
 	// Verify error names
 	for (const errorName in config.errors) {
 		validateIdentifier(errorName, Casing.Snake);
@@ -188,6 +354,7 @@ export async function loadModule(
 		registry,
 		scripts,
 		actors,
+		routes,
 		db,
 	};
 }

src/runtime/runtime.ts
@@ -1,12 +1,13 @@
 import { addFormats, Ajv } from "./deps.ts";
 import { ScriptContext } from "./context.ts";
-import { Context, TestContext } from "./context.ts";
+import { Context, TestContext, RouteContext } from "./context.ts";
 import { Postgres, PrismaClientDummy } from "./postgres.ts";
-import { handleRequest } from "./server.ts";
+import { serverHandler } from "./server.ts";
 import { TraceEntryType } from "./trace.ts";
 import { newTrace } from "./trace.ts";
 import { RegistryCallMap } from "./proxy.ts";
 import { ActorDriver } from "./actor.ts";
+import { QualifiedPathPair } from "./path_resolver.ts";
 
 export interface Config {
 	modules: Record<string, Module>;
@@ -15,6 +16,7 @@ export interface Config {
 export interface Module {
 	scripts: Record<string, Script>;
 	actors: Record<string, Actor>;
+	routes: Record<string, Route>;
 	errors: Record<string, ErrorConfig>;
 	db?: {
 		name: string;
@@ -39,6 +41,23 @@ export interface Script {
 	public: boolean;
 }
 
+export interface RouteBase {
+	// deno-lint-ignore no-explicit-any
+	run: RouteRun<any, any>;
+	methods: Set<string>;
+}
+
+export interface PrefixRoute extends RouteBase {
+	pathPrefix: string;
+}
+
+export interface ExactRoute extends RouteBase {
+	path: string;
+}
+
+export type Route = ExactRoute | PrefixRoute;
+
+
 export type ScriptRun<Req, Res, UserConfigT, DatabaseT> = (
 	ctx: ScriptContext<any, any, any, any, UserConfigT, DatabaseT>,
 	req: Req,
@@ -48,6 +67,11 @@ export interface Actor {
 	actor: any;
 	storageId: string;
 }
+
+export type RouteRun<UserConfigT, DatabaseT> = (
+	ctx: RouteContext<any, any, any, any, UserConfigT, DatabaseT>,
+	req: Request,
+) => Promise<Response>;
 
 export interface ErrorConfig {
 	description?: string;
@@ -62,7 +86,7 @@ export class Runtime<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, Actor
 		public config: Config,
 		public actorDriver: ActorDriver,
 		private dependencyCaseConversionMap: RegistryCallMap,
-		private actorDependencyCaseConversionMap: RegistryCallMap,
+		private actorCaseConversionMap: RegistryCallMap,
 	) {
 		this.postgres = new Postgres();
 
@@ -80,7 +104,26 @@ export class Runtime<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, Actor
 	public createRootContext(
 		traceEntryType: TraceEntryType
 	): Context<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, ActorsCamelT> {
-		return new Context(this, newTrace(traceEntryType), this.dependencyCaseConversionMap, this.actorDependencyCaseConversionMap);
+		return new Context(this, newTrace(traceEntryType), this.dependencyCaseConversionMap, this.actorCaseConversionMap);
+	}
+
+	public createRootRouteContext(
+		traceEntryType: TraceEntryType,
+		moduleName: string,
+		routeName: string,
+	): RouteContext<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, ActorsCamelT, unknown, PrismaClientDummy | undefined> {
+		const module = this.config.modules[moduleName];
+		if (!module) throw new Error(`Module not found: ${moduleName}`);
+
+		return new RouteContext(
+			this,
+			newTrace(traceEntryType),
+			moduleName,
+			this.postgres.getOrCreatePool(module)?.prisma,
+			routeName,
+			this.dependencyCaseConversionMap,
+			this.actorCaseConversionMap
+		);
 	}
 
 	/**
@@ -89,10 +132,8 @@ export class Runtime<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, Actor
 	public async serve() {
 		const port = parseInt(Deno.env.get("PORT") ?? "8080");
 		console.log(`Serving on port ${port}`);
-		await Deno.serve(
-			{ port },
-			(req, info) => handleRequest(this, req, { remoteAddress: info.remoteAddr.hostname }),
-		).finished;
+
+		await Deno.serve({ port }, serverHandler(this)).finished;
 	}
 
 	/**
@@ -161,4 +202,33 @@ export class Runtime<DependenciesSnakeT, DependenciesCamelT, ActorsSnakeT, Actor
 			},
 		});
 	}
+
+	
+
+	public routePaths(): QualifiedPathPair[] {
+		const paths: QualifiedPathPair[] = [];
+		for (const moduleName in this.config.modules) {
+			const module = this.config.modules[moduleName];
+			for (const routeName in module.routes) {
+				const route = module.routes[routeName];
+				if ("path" in route) {
+					paths.push({
+						module: moduleName,
+						route: routeName,
+						path: { path: route.path, isPrefix: false },
+					});
+				} else {
+					paths.push({
+						module: moduleName,
+						route: routeName,
+						path: { path: route.pathPrefix, isPrefix: true },
+					});
+				}
+			}
+		}
+
+		return paths;
+	}
 }
+
+export type PathPair = { path: string; isPrefix: boolean };

rivet-gg/modules

src/project/module.ts
@@ -4,13 +4,70 @@ import { configPath as moduleConfigPath, readConfig as readModuleConfig } from "
 import { ModuleConfig } from "../config/module.ts";
 import { Script } from "./script.ts";
 import { Actor } from "./actor.ts";
+import { Route } from "./route.ts";
 import { Project } from "./project.ts";
 import { Registry } from "./registry.ts";
 import { validateIdentifier } from "../types/identifiers/mod.ts";
 import { Casing } from "../types/identifiers/defs.ts";
-import { ProjectModuleConfig } from "../config/project.ts";
+import { configPath as projectConfigPath, ProjectModuleConfig } from "../config/project.ts";
 import { UserError } from "../error/mod.ts";
 import { AnySchemaElement } from "../build/schema/mod.ts";
+import { RouteConfig } from "../config/module.ts";
+
+/**
+ * Validates a path provided by the user.
+ *
+ * If the path is invalid, a {@linkcode UserError} is thrown, otherwise the
+ * function returns nothing.
+ *
+ * Paths must:
+ * - Start with a forward slash
+ * - IF they are a prefix, end with a forward slash
+ * - IF they are an exact path, NOT end with a forward slash
+ *
+ * @param path The user-provided http path
+ * @param isPrefix Whether or not the path should be treated as a prefix
+ * @param configPath Where the (possibly) offending config file is located
+ * @throws {UserError} if the path is invalid
+ */
+function validatePath(
+	path: string,
+	isPrefix: boolean,
+	configPath: string,
+): void {
+	// Ensure path starts with a forward slash
+	if (!path.startsWith("/")) {
+		throw new UserError(
+			"Route paths must start with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify("/" + path)}`,
+			},
+		);
+	}
+
+	const hasTrailingSlash = path.endsWith("/");
+	if (isPrefix && !hasTrailingSlash) {
+		throw new UserError(
+			"Prefix paths must end with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify(path + "/")}`,
+			},
+		);
+	} else if (!isPrefix && hasTrailingSlash) {
+		throw new UserError(
+			"Exact paths must not end with a forward slash",
+			{
+				path: configPath,
+				details: `Got ${JSON.stringify(path)}`,
+				suggest: `Change this to ${JSON.stringify(path.replace(/\/$/, ""))}`,
+			},
+		);
+	}
+}
 
 export interface Module {
 	/**
@@ -52,6 +109,7 @@ export interface Module {
 
 	scripts: Map<string, Script>;
 	actors: Map<string, Actor>;
+	routes: Map<string, Route>;
 	db?: ModuleDatabase;
 
 	// Cache
@@ -64,6 +122,7 @@ export interface ModuleDatabase {
 }
 
 export async function loadModule(
+	projectRoot: string,
 	modulePath: string,
 	name: string,
 	projectModuleConfig: ProjectModuleConfig,
@@ -79,6 +138,50 @@ export async function loadModule(
 	const storageAlias = projectModuleConfig.storageAlias ?? name;
 	validateIdentifier(storageAlias, Casing.Snake);
 
+	const scripts = await loadScripts(modulePath, config);
+	const actors = await loadActors(modulePath, config);
+	const routes = await loadRoutes(modulePath, config, name, projectRoot, projectModuleConfig);
+
+	// Verify error names
+	for (const errorName in config.errors) {
+		validateIdentifier(errorName, Casing.Snake);
+	}
+
+	// Load db config
+	let db: ModuleDatabase | undefined = undefined;
+	if (await exists(resolve(modulePath, "db"), { isDirectory: true })) {
+		db = {
+			schema: `module_${storageAlias}`,
+		};
+	}
+
+	// Merge user config with default config
+	const userConfig = deepMerge(
+		config.defaultConfig ?? {},
+		projectModuleConfig.config ?? {},
+		{
+			arrays: "replace",
+			maps: "merge",
+			sets: "replace",
+		},
+	);
+
+	return {
+		path: modulePath,
+		name,
+		projectModuleConfig,
+		userConfig,
+		config,
+		registry,
+		storageAlias,
+		scripts,
+		actors,
+		routes,
+		db,
+	};
+}
+
+async function loadScripts(modulePath: string, config: ModuleConfig) {
 	// Find names of the expected scripts to find. Used to print error for extra scripts.
 	const scriptsPath = resolve(modulePath, "scripts");
 	const expectedScripts = new Set(
@@ -127,6 +230,10 @@ export async function loadModule(
 		);
 	}
 
+	return scripts;
+}
+
+async function loadActors(modulePath: string, config: ModuleConfig) {
 	// Find names of the expected actors to find. Used to print error for extra actors.
 	const actorsPath = resolve(modulePath, "actors");
 	const expectedActors = new Set(
@@ -191,42 +298,124 @@ export async function loadModule(
 		}
 	}
 
-	// Verify error names
-	for (const errorName in config.errors) {
-		validateIdentifier(errorName, Casing.Snake);
-	}
+	return actors;
+}
 
-	// Load db config
-	let db: ModuleDatabase | undefined = undefined;
-	if (await exists(resolve(modulePath, "db"), { isDirectory: true })) {
-		db = {
-			schema: `module_${storageAlias}`,
-		};
-	}
-
-	// Merge user config with default config
-	const userConfig = deepMerge(
-		config.defaultConfig ?? {},
-		projectModuleConfig.config ?? {},
-		{
-			arrays: "replace",
-			maps: "merge",
-			sets: "replace",
-		},
+async function loadRoutes(
+	modulePath: string,
+	config: ModuleConfig,
+	moduleName: string,
+	projectRoot: string,
+	projectModuleConfig: ProjectModuleConfig,
+) {
+	// Read routes
+	const routesPath = resolve(modulePath, "routes");
+	const expectedRoutes = new Set(
+		await glob.glob("*.ts", { cwd: resolve(modulePath, "routes") }),
 	);
 
-	return {
-		path: modulePath,
-		name,
-		projectModuleConfig,
-		userConfig,
-		config,
-		registry,
-		storageAlias,
-		scripts,
-		actors,
-		db,
-	};
+	const routes = new Map<string, Route>();
+	if (config.routes) {
+		let rawPathPrefix: string;
+		if (projectModuleConfig.routes?.pathPrefix) {
+			validatePath(
+				projectModuleConfig.routes.pathPrefix,
+				true,
+				projectConfigPath(projectRoot),
+			);
+
+			rawPathPrefix = projectModuleConfig.routes.pathPrefix;
+		} else {
+			// Default to /modules/{module}/route/ for the path prefix
+			const prefix = `/modules/${moduleName}/route/`;
+			validatePath(
+				prefix,
+				true,
+				projectConfigPath(projectRoot),
+			);
+
+			rawPathPrefix = prefix;
+		}
+
+		// Remove the trailing slash
+		const pathPrefix = rawPathPrefix.replace(/\/$/, "");
+
+		for (const [routeName, relativeRouteConfig] of Object.entries(config.routes)) {
+			validateIdentifier(routeName, Casing.Snake);
+
+			// Load script
+			const routeScriptPath = resolve(
+				routesPath,
+				routeName + ".ts",
+			);
+			if (!await exists(routeScriptPath)) {
+				throw new UserError(
+					`Route not found at ${relative(Deno.cwd(), routeScriptPath)}.`,
+					{
+						suggest: "Check the routes in the module.yaml are configured correctly.",
+						path: moduleConfigPath(modulePath),
+					},
+				);
+			}
+
+			// Get subpath (either path or pathPrefix)
+			let subpath: string;
+			if ("path" in relativeRouteConfig) {
+				validatePath(
+					relativeRouteConfig.path,
+					false,
+					moduleConfigPath(modulePath),
+				);
+
+				// Remove leading slash
+				subpath = relativeRouteConfig.path.replace(/^\//, "");
+			} else {
+				validatePath(
+					relativeRouteConfig.pathPrefix,
+					true,
+					moduleConfigPath(modulePath),
+				);
+
+				// Remove leading and trailing slashes
+				subpath = relativeRouteConfig.pathPrefix.replace(/^\//, "").replace(/\/$/, "");
+			}
+
+			// Create route config with absolute path
+			let routeConfig: RouteConfig;
+			if ("path" in relativeRouteConfig) {
+				routeConfig = {
+					...relativeRouteConfig,
+					path: `${pathPrefix}/${subpath}`,
+				};
+			} else {
+				routeConfig = {
+					...relativeRouteConfig,
+					pathPrefix: `${pathPrefix}/${subpath}`,
+				};
+			}
+
+			const route: Route = {
+				path: routesPath,
+				name: routeName,
+				config: routeConfig,
+			};
+			routes.set(routeName, route);
+
+			// Remove script
+			expectedRoutes.delete(routeName + ".ts");
+		}
+	}
+
+	// Throw error extra routes
+	if (expectedRoutes.size > 0) {
+		const routeList = Array.from(expectedRoutes).map((x) => `- ${resolve(routesPath, x)}\n`);
+		throw new UserError(
+			`Found extra routes not registered in module.yaml.`,
+			{ details: routeList.join(""), suggest: "Add these routes to the module.yaml file.", path: routesPath },
+		);
+	}
+
+	return routes;
 }
 
 export function moduleHelperGen(

src/runtime/runtime.ts
@@ -1,12 +1,13 @@
 import { ModuleContextParams, ScriptContext } from "./context.ts";
-import { Context, TestContext } from "./context.ts";
+import { Context, RouteContext, TestContext } from "./context.ts";
 import { PgPoolDummy, Postgres, PrismaClientDummy } from "./postgres.ts";
-import { handleRequest } from "./server.ts";
+import { serverHandler } from "./server.ts";
 import { TraceEntryType } from "./trace.ts";
 import { newTrace } from "./trace.ts";
 import { RegistryCallMap } from "./proxy.ts";
 import { ActorDriver } from "./actor/driver.ts";
 import { ActorBase } from "./actor/actor.ts";
+import { QualifiedPathPair } from "./path_resolver.ts";
 import { ContextParams } from "./mod.ts";
 import { errorToLogEntries, log, LOGGER_CONFIG } from "./logger.ts";
 import { Environment } from "./environment.ts";
@@ -52,6 +53,7 @@ export interface Module {
 	storageAlias: string;
 	scripts: Record<string, Script>;
 	actors: Record<string, Actor>;
+	routes: Record<string, Route>;
 	errors: Record<string, ErrorConfig>;
 	db?: {
 		/** Name of the Postgres schema the tables live in. */
@@ -74,6 +76,22 @@ export interface Script {
 	public: boolean;
 }
 
+export interface RouteBase {
+	// deno-lint-ignore no-explicit-any
+	run: RouteRun<any, any, any>;
+	methods: Set<string>;
+}
+
+export interface PrefixRoute extends RouteBase {
+	pathPrefix: string;
+}
+
+export interface ExactRoute extends RouteBase {
+	path: string;
+}
+
+export type Route = ExactRoute | PrefixRoute;
+
 export type ScriptRun<Req, Res, UserConfigT, DatabaseT, DatabaseSchemaT> = (
 	ctx: ScriptContext<{
 		dependenciesSnake: any;
@@ -95,6 +113,19 @@ export interface Actor {
 	storageAlias: string;
 }
 
+export type RouteRun<UserConfigT, DatabaseT, DatabaseSchemaT> = (
+	ctx: RouteContext<{
+		dependenciesSnake: any;
+		dependenciesCamel: any;
+		actorsSnake: any;
+		actorsCamel: any;
+		userConfig: UserConfigT;
+		database: DatabaseT;
+		databaseSchema: DatabaseSchemaT;
+	}>,
+	req: Request,
+) => Promise<Response>;
+
 export interface ErrorConfig {
 	description?: string;
 }
@@ -111,7 +142,7 @@ export class Runtime<Params extends ContextParams> {
 		public readonly config: Config,
 		public actorDriver: ActorDriver,
 		private dependencyCaseConversionMap: RegistryCallMap,
-		private actorDependencyCaseConversionMap: RegistryCallMap,
+		private actorCaseConversionMap: RegistryCallMap,
 	) {
 		// Read config
 		this.hostname = env.get("OPENGB_HOSTNAME") ?? "127.0.0.1";
@@ -142,12 +173,56 @@ export class Runtime<Params extends ContextParams> {
 
 	public createRootContext(
 		traceEntryType: TraceEntryType,
-	): Context<{ dependenciesSnake: Params["dependenciesSnake"]; dependenciesCamel: Params["dependenciesCamel"] }> {
+	): Context<{
+		dependenciesSnake: Params["dependenciesSnake"];
+		dependenciesCamel: Params["dependenciesCamel"];
+		actorsSnake: Params["actorsSnake"];
+		actorsCamel: Params["actorsCamel"];
+	}> {
 		return new Context(
 			this,
 			newTrace(traceEntryType, this.config.runtime),
 			this.dependencyCaseConversionMap,
-			this.actorDependencyCaseConversionMap,
+			this.actorCaseConversionMap,
+		);
+	}
+
+	public createRouteContext(
+		ctx: Context<Params>,
+		moduleName: string,
+		routeName: string,
+	) {
+		// FIXME: This is a pretty terrible hack. We should find a better way to
+		// do this, probably with a "public" underscore function.
+		const ctxWithPublicGRC = ctx as unknown as { getRouteContext: Context<Params>["getRouteContext"] };
+		return ctxWithPublicGRC.getRouteContext(moduleName, routeName);
+	}
+
+	public createRootRouteContext(
+		traceEntryType: TraceEntryType,
+		moduleName: string,
+		routeName: string,
+	): RouteContext<{
+		"dependenciesSnake": Params["dependenciesSnake"];
+		"dependenciesCamel": Params["dependenciesCamel"];
+		"actorsSnake": Params["actorsSnake"];
+		"actorsCamel": Params["actorsCamel"];
+		"userConfig": any;
+		"database": PrismaClientDummy | undefined;
+		"databaseSchema": any;
+	}> {
+		const module = this.config.modules[moduleName];
+		if (!module) throw new Error(`Module not found: ${moduleName}`);
+
+		return new RouteContext(
+			this,
+			newTrace(traceEntryType),
+			moduleName,
+			this.postgres.getOrCreatePrismaClient(this.config, module),
+			module.db?.schema,
+			routeName,
+			this.dependencyCaseConversionMap,
+			this.actorCaseConversionMap,
 		);
 	}
 
@@ -169,7 +244,7 @@ export class Runtime<Params extends ContextParams> {
 					);
 				},
 			},
-			(req, info) => handleRequest(this, req, { remoteAddress: info.remoteAddr.hostname }),
+			serverHandler(this),
 		).finished;
 	}
 
@@ -302,4 +377,28 @@ export class Runtime<Params extends ContextParams> {
 		if (!origin) return true;
 		return this.config.cors?.origins.has(origin) ?? false;
 	}
+
+	public routePaths(): QualifiedPathPair[] {
+		const paths: QualifiedPathPair[] = [];
+		for (const [moduleName, module] of Object.entries(this.config.modules)) {
+			for (const [routeName, route] of Object.entries(module.routes)) {
+				if ("path" in route) {
+					paths.push({
+						module: moduleName,
+						route: routeName,
+						path: { path: route.path, isPrefix: false },
+					});
+				} else {
+					paths.push({
+						module: moduleName,
+						route: routeName,
+						path: { path: route.pathPrefix, isPrefix: true },
+					});
+				}
+			}
+		}
+		return paths;
+	}
 }
+
+export type PathPair = { path: string; isPrefix: boolean };