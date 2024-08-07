1253b69d38cf91258afdbb9f486ac3303c963b27
Nathan Flurry <developer@nathanflurry.com>
2024-08-06 18:03:04 -0700

    feat(sandbox): create sandbox foundation

---
+++ sandbox/README.md
@@ -0,0 +1,25 @@
+# Sandbox
+
+## Running Locally
+
+```sh
+./scripts/run_local.sh
+```
+
+Visit http://localhost:8080
+
+## Running Remotely
+
+```sh
+./scripts/deploy.sh
+./scripts/run_remote.sh
+```
+
+Visit http://localhost:8080
+
+## Updating SDK
+
+```sh
+./scripts/gen_sdk.ts
+```
+
--- tests/basic/test.sh
+++ tests/basic/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X POST "http://localhost:6420/modules/auth/scripts/send_email_verification/call" \
+curl -X POST "https://test-1-backe-1hv--staging.backend.nathan16.gameinc.io/modules/auth/scripts/send_email_verification/call" \
   -H "Content-Type: application/json" \
   -d '{
     "email": "test@rivet.gg"
