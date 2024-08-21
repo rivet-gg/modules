cab6396f9f36cae76b4581db5fe545b1fef6a439
Nathan Flurry <NathanFlurry@users.noreply.github.com>
2024-08-21 01:41:32 +0000

    chore: add sandbox scripts (#141)

---
+++ sandbox/scripts/fetch_state.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S deno run -A
+
+const origin = Deno.env.get("OPENGB_ORIGIN");
+
+const res = await fetch(`${origin}/modules/lobbies/scripts/fetch_lobby_manager_state/call`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({}),
+});
+if (res.status != 200) {
+  console.error(await res.text());
+  Deno.exit(1);
+}
+
+console.log(await res.json());
+
