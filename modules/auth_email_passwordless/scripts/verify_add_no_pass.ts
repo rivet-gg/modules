22132b748f0af1e2e473e20625fba65b3da4d679
Nathan Flurry <NathanFlurry@users.noreply.github.com>
2024-08-24 11:54:11 -0700

    chore: remove enable from auth* configs (#145)

--- modules/auth_email_passwordless/scripts/verify_add_no_pass.ts
+++ modules/auth_email_passwordless/scripts/verify_add_no_pass.ts
@@ -9,6 +9,5 @@ export async function run(
 	ctx: ScriptContext,
 	req: Request,
 ): Promise<Response> {
-	if (!ctx.config.enable) throw new RuntimeError("provider_disabled");
 	return await ctx.modules.authEmail.verifyAddNoPass(req);
 }
