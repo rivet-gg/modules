d71eda0bc30c38ebc4e98b3b792322f881115cdc
Nathan Flurry <developer@nathanflurry.com>
2024-09-03 23:35:39 -0700

    chore: stop postgres instance on clean

--- packages/toolchain/project/project.ts
+++ packages/toolchain/project/project.ts
@@ -14,6 +14,9 @@ import { UnreachableError, UserError } from "../error/mod.ts";
 import { Runtime } from "../build/mod.ts";
 import { PathResolver, QualifiedPathPair } from "../../path_resolver/mod.ts";
 import { RouteCollisionError } from "../error/mod.ts";
+import { stop } from "../postgres/manager.ts";
+import { getDefaultPostgresManager } from "../postgres/mod.ts";
+import { verbose } from "../term/status.ts";
 
 export interface Project {
 	path: string;
@@ -427,6 +430,15 @@ export async function listSourceFiles(
 }
 
 export async function cleanProject(project: Project) {
+	// Delete database
+  verbose("Stopping Postgres");
+	const postgresManager = await getDefaultPostgresManager(project);
+	if (postgresManager) {
+		stop(postgresManager);
+	}
+
+	// Delete project files
+  verbose("Removing project gen path");
 	await Deno.remove(projectGenPath(project), { recursive: true });
 }
 
