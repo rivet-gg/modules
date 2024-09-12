rivet-gg/opengb

internal api router

packages/cli/src/commands/dev.ts
@@ -1,4 +1,4 @@
-import { Command } from "../deps.ts";
+import { Command, Mutex } from "../deps.ts";
 import { GlobalOpts, initProject } from "../common.ts";
 import { build, DbDriver, Format, Runtime } from "../../../toolchain/src/build/mod.ts";
 import { ensurePostgresRunning } from "../../../toolchain/src/utils/postgres_daemon.ts";
@@ -6,6 +6,7 @@ import { watch } from "../../../toolchain/src/watch/mod.ts";
 import { Project } from "../../../toolchain/src/project/mod.ts";
 import { InternalError } from "../../../toolchain/src/error/mod.ts";
 import { ENTRYPOINT_PATH, projectGenPath } from "../../../toolchain/src/project/project.ts";
+import { createProjectInternalApiRouter } from "../../../toolchain/src/internal-api/mod.ts";
 
 export const devCommand = new Command<GlobalOpts>()
 	.description("Start the development server")
@@ -16,10 +17,22 @@ export const devCommand = new Command<GlobalOpts>()
 	.option("--force-deploy-migrations", "Auto deploy migrations without using development prompt", { default: false })
 	.action(
 		async (opts) => {
+			const project = await initProject(opts);
+
+			const mutex = new Mutex();
+
+			await mutex.acquire();
+
+			const internalApiRouter = createProjectInternalApiRouter(project, mutex);
+			Deno.serve({ port: 6421, handler: internalApiRouter.fetch, onListen: () => {} });
+
 			await watch({
 				loadProjectOpts: opts,
 				disableWatch: !opts.watch,
 				async fn(project: Project, signal: AbortSignal) {
+					if (!mutex.isLocked()) {
+						await mutex.acquire();
+					}
 					await ensurePostgresRunning(project);
 
 					// Build project
@@ -46,6 +59,7 @@ export const devCommand = new Command<GlobalOpts>()
 					];
 					if (opts.check) args.push("--check");
 
+					mutex.release();
 					// Run entrypoint
 					const entrypointPath = projectGenPath(project, ENTRYPOINT_PATH);
 					const cmd = await new Deno.Command("deno", {
@@ -57,8 +71,8 @@ export const devCommand = new Command<GlobalOpts>()
 						stdout: "inherit",
 						stderr: "inherit",
 						signal,
-					})
-						.output();
+					}).output();
+
 					if (!cmd.success) throw new InternalError("Entrypoint failed", { path: entrypointPath });
 				},
 			});

rivet-gg/modules

packages/cli/commands/dev.ts
@@ -1,5 +1,5 @@
 import { Command } from "@cliffy/command";
-import { GlobalOpts } from "../common.ts";
+import { GlobalOpts, initProject } from "../common.ts";
 import { build, DbDriver, Format, Runtime } from "../../toolchain/build/mod.ts";
 import { watch } from "../../toolchain/watch/mod.ts";
 import { Project } from "../../toolchain/project/mod.ts";
@@ -25,10 +25,32 @@ export const devCommand = new Command<GlobalOpts>()
 	.option("--non-interactive", "Run without interactive input")
 	.action(
 		async (opts) => {
+			const project = await initProject(opts);
+
+			const mutex = new Mutex();
+
+			await mutex.acquire();
+
+			const internalApiRouter = createProjectInternalApiRouter(project, mutex);
+
+			const hostname = Deno.env.get("OPENGB_EDITOR_HOST") ?? "127.0.0.1";
+			const port = parseInt(Deno.env.get("OPENGB_EDITOR_PORT") ?? "6421");
+			Deno.serve({
+				hostname,
+				port,
+				handler: internalApiRouter.fetch,
+				onListen: () => {
+					progress("OpenGB Editor started", `http://${hostname}:${port}`);
+				},
+			});
+
 			await watch({
 				loadProjectOpts: opts,
 				disableWatch: !opts.watch,
 				async fn(project: Project, signal: AbortSignal) {
+					if (!mutex.isLocked()) {
+						await mutex.acquire();
+					}
 					await ensurePostgresRunning(project);
 
 					// Build project
@@ -57,6 +79,7 @@ export const devCommand = new Command<GlobalOpts>()
 					];
 					if (opts.check) args.push("--check");
 
+					mutex.release();
 					// Run entrypoint
 					const entrypointPath = projectGenPath(project, ENTRYPOINT_PATH);
 					const cmd = await new Deno.Command("deno", {