1f8d48a8d54e6acc314d26fac5500b38a1f4742a
ABCxFF <79597906+ABCxFF@users.noreply.github.com>
2024-09-19 17:04:04 -0400

    fix(lobbies-regions): Use proper distance calculations for (long, lat) coords

--- modules/lobbies/utils/region.ts
+++ modules/lobbies/utils/region.ts
@@ -8,7 +8,13 @@ import { REGIONS as LOCAL_DEVELOPMENT_REGIONS } from "./lobby/backend/local_deve
 import { UnreachableError } from "../module.gen.ts";
 import { LobbyBackend } from "../config.ts";
 
-export interface Region {
+export interface RegionGeoCoords {
+  latitude: number;
+  longitude: number;
+}
+
+
+export interface Region extends RegionGeoCoords {
   slug: string;
   name: string;
   latitude: number;
@@ -22,3 +28,46 @@ export function regionsForBackend(backend: LobbyBackend): Region[] {
   else throw new UnreachableError(backend);
 }
 
+// Using haversine formula to calculate approximate distance
+function getDistance(a: RegionGeoCoords, b: RegionGeoCoords) {
+  const EARTH_RADIUS = 6371; // Radius of the earth in km
+  const DEG_2_RAD = (Math.PI / 180);
+
+  const deltaLatitude = DEG_2_RAD * (b.latitude - a.latitude);
+  const deltaLongitude = DEG_2_RAD * (b.longitude - a.longitude);
+  
+  const aLatitude = DEG_2_RAD * a.latitude;
+  const bLatitude = DEG_2_RAD * b.latitude;
+
+  const sinOfDLat = Math.sin(deltaLatitude / 2);
+  const sinOfDLon = Math.sin(deltaLongitude / 2);
+  const x = sinOfDLat * sinOfDLat + sinOfDLon * sinOfDLon * Math.cos(aLatitude) * Math.cos(bLatitude);
+  const unitDistance = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x)); 
+
+  return EARTH_RADIUS * unitDistance;
+}
+
+export function getClosestRegion(
+  region: Region[],
+  coords: RegionGeoCoords
+) {
+  let closestRegion: Region | null = null;
+  let closestDistance = Infinity;
+  for (const r of region) {
+    const distance = getDistance(r, coords);
+    if (distance < closestDistance) {
+      closestRegion = r;
+      closestDistance = distance;
+    }
+  }
+  return closestRegion;
+}
+
+export function getSortedRegionsByProximity(
+  regions: Region[],
+  coords: RegionGeoCoords
+) {
+  return [...regions].sort((a, b) => {
+    return getDistance(a, coords) - getDistance(b, coords);
+  });
+}
---
+++ modules/lobbies/utils/rivet/geo_coord.ts
@@ -0,0 +1,44 @@
+import { ScriptContext } from "../../module.gen.ts";
+import { RegionGeoCoords } from "../region.ts";
+
+export function getRequestGeoCoords(
+    ctx: ScriptContext
+): RegionGeoCoords | undefined {
+    // If they aren't real servers, we're probably not in managed
+    // So we just return
+    if (!("server" in ctx.config.lobbies.backend)) {
+        return undefined;
+    }
+
+    if (ctx.environment.get("RIVET_BACKEND_RUNTIME") !== "cloudflare_workers_platforms") {
+        return undefined;
+    }
+
+    const topLevelTrace = ctx.trace.entries[0];
+    if (!topLevelTrace || !("httpRequest" in topLevelTrace.type)) {
+        return undefined;
+    }
+
+    const httpReq = topLevelTrace.type.httpRequest
+    if (!httpReq) {
+        return undefined;
+    }
+
+    if (!httpReq.headers["x-backend-client-coords"]) {
+        throw new Error("Missing x-backend-client-coords header");
+    }
+
+    const [latitudeStr, longitudeStr] = httpReq.headers["x-backend-client-coords"].split(",") as [string, string];
+
+    const longitude = parseFloat(longitudeStr.trim());
+    const latitude = parseFloat(latitudeStr.trim());
+
+    if (!isFinite(longitude) || !isFinite(latitude)) {
+        throw new Error("Invalid x-backend-client-coords header (non-finite)");
+    }
+
+    return {
+        latitude,
+        longitude
+    }
+}
